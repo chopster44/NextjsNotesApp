e0d1872209ace40a1e5e9d6155d14bf39c99e0d4
chopster44 <oscar@chopster44.com>
2022-11-12 11:49:10 +0000

    Added prettier

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,14 +1,17 @@
-import CreateNote from "./CreateNote";
-import styles from "../../styles/Notes.module.css";
+import CreateNote from './CreateNote';
+import styles from '../../styles/Notes.module.css';
 import Note from './Note';
 
 export const db = {
 	route: 'http://192.168.0.12:8090',
-	collectionName: 'notes1'
+	collectionName: 'notes1',
 };
 
 async function getNotes() {
-	const client = await fetch(`${db.route}/api/collections/${db.collectionName}/records`, {cache: "no-store"});
+	const client = await fetch(
+		`${db.route}/api/collections/${db.collectionName}/records`,
+		{ cache: 'no-store' }
+	);
 	const data = await client.json();
 	return data?.items as any[];
 }
