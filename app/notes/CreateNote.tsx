46b8029245a0f53e230681e263887eeb7b2fc9d2
chopster44 <oscar@chopster44.com>
2022-11-12 15:11:35 +0000

    Added tags

--- app/notes/CreateNote.tsx
+++ app/notes/CreateNote.tsx
@@ -1,39 +1,43 @@
 'use client';
 
-import { useState } from "react";
+import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import styles from "../../styles/Notes.module.css";
-import EditorBar from "./EditorBar";
-import {db} from "./page";
-
+import styles from '../../styles/Notes.module.css';
+import EditorBar from './EditorBar';
+import { db } from './page';
 
 export default function CreateNote() {
-	const [title, setTitle] = useState("");
-	const [content, setContent] = useState("");
+	const [title, setTitle] = useState('');
+	const [content, setContent] = useState('');
 
 	const router = useRouter();
 
 	const create = async () => {
-		await fetch(`${db.route}/api/collections/${db.collectionName}/records`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				title,
-				content,
-			}),
-		});
+		await fetch(
+			`${db.route}/api/collections/${db.collectionName}/records`,
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					title: title,
+					content: content,
+					hidden: false,
+					tags: '-',
+				}),
+			}
+		);
 
-		setContent("");
-		setTitle("");
+		setContent('');
+		setTitle('');
 
 		router.refresh();
-	}
+	};
 	const cancel = () => {
-		setContent("");
-		setTitle("");
-	}
+		setContent('');
+		setTitle('');
+	};
 
 	return (
 		<form className={styles.note}>
